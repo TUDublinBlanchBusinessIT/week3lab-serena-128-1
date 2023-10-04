405b4a1c5bb1478dfca43a429512ed22388bc9d4
B00141403 Serena Emedeke <B00141403@mytudublin.ie>
2023-10-04 10:07:35 +0100

    changes 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,9 +1,10 @@
 <?php
-#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
+#phpForLoop 
+#Serena Emedeke 4/10/2023
 #put your name and the date here
 #put a descripition of what the code does here - 
 #this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+for ($i=1;$i<=20;$i++) {
     echo "$i, ";
 }
 
